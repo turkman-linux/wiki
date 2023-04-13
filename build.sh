2dc7bc5bed174b8d5caeddd9f24d62cb8ce0193e
sulincix <parduscix@yandex.ru>
2023-04-13 21:05:37 +0000

    font install CI generate

--- build.sh
+++ build.sh
@@ -17,7 +17,7 @@ EOF
 for sayfa in bash-dersi vala-dersi python-dersi makefile-dersi git-kullanimi docker-kullanimi ; do
     curl https://gitlab.com/sulincix/sayfalar/-/raw/master/rst/$sayfa.rst > source/99-kodlama/$sayfa-dersi.rst
 done
-make html
+make html pdf
 find build/html -type f -iname *.html -exec sed -i "s/.*src=\".*\.js\".*//g" {} \;
 find build/html -type f -iname *.js -exec rm -rf {} \;
 rm -rf build/html/_static/css build/html/{search,genindex}.html
