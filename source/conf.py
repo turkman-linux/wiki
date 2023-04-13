55eea45a60929dc8075fbf7a362dcd93c1be7de9
sulincix <parduscix@yandex.ru>
2023-04-13 21:32:26 +0000

    fff

--- source/conf.py
+++ source/conf.py
@@ -3,17 +3,14 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
 project = 'Türkmen Wiki'
 copyright = 'Türkmen Linux 2023'
 author = 'turkman'
-
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
+language = 'tr'
 extensions = ['sphinx.ext.autodoc', 'rst2pdf.pdfbuilder']
+
+# -- Options for PDF output --
+
 pdf_documents = [('index', u'rst2pdf', u'Türkmen Dokümanı', u'Türkmen Linux'),]
 pdf_stylesheets = ['style-main.yaml', 'tango']
 pdf_font_path = ['/usr/share/fonts', '/usr/share/texmf-dist/fonts/']
@@ -23,15 +20,12 @@ pdf_use_toc = True
 pdf_default_dpi = 96
 pdf_compressed = True
 pdf_language = "tr_TR"
-language = 'tr'
 
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+# -- Options for HTML output --
 
 html_theme = 'alabaster'
-
 html_static_path = ['_static']
-
 html_theme_options = {
     'font_family' : 'monospace',
     'footnote_bg': 'none',
