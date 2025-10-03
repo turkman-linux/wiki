41d3845e6423ab94be000463617a0b4c8e3bc4de
Ali Riza KESKIN <aliriza.keskin@pardus.org.tr>
2025-10-03 12:34:38 +0000

    use py3.13

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 # install dependencies
 set -ex
 ymp repo --update --ignore-gpg
-ymp it make python3.12 python fontconfig py3-cairo brotli --upgrade --no-emerge --ignore-quarantine --jobs=1 
-pydefault 3.12
-python3.12 -m ensurepip
-pip3.12 install sphinx
-pip3.12 install rst2pdf
-pip3.12 install sphinx-sitemap
+ymp it make python3.13 python fontconfig py3-cairo brotli --upgrade --no-emerge --ignore-quarantine --jobs=1
+pydefault 3.13
+python3.13 -m ensurepip
+pip3.13 install sphinx
+pip3.13 install rst2pdf
+pip3.13 install sphinx-sitemap
 # install font
 wget https://github.com/dejavu-fonts/dejavu-fonts/releases/download/version_2_37/dejavu-fonts-ttf-2.37.zip -O font.zip
 unzip font.zip
