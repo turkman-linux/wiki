81c473e92d66a701c7833a1004b4405ce39c7d88
Ali Riza KESKIN <aliriza.keskin@pardus.org.tr>
2026-05-06 15:51:21 +0300

    wiki build py3.14

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,12 @@
 # install dependencies
 set -ex
 ymp repo --update --ignore-gpg
-ymp it make python3.13 python fontconfig py3-cairo brotli --upgrade --no-emerge --ignore-quarantine --jobs=1
-pydefault 3.13
-python3.13 -m ensurepip
-pip3.13 install sphinx
-pip3.13 install rst2pdf
-pip3.13 install simple-sphinx-xml-sitemap
-# install font
-wget https://github.com/dejavu-fonts/dejavu-fonts/releases/download/version_2_37/dejavu-fonts-ttf-2.37.zip -O font.zip
-unzip font.zip
-mv dejavu-*/ttf /usr/share/fonts/
-fc-cache -fv
+ymp it make python3.14 python fontconfig py3-cairo brotli dejavu --upgrade --no-emerge --ignore-quarantine --jobs=1
+pydefault 3.14
+python3.14 -m ensurepip
+pip3.14 install sphinx
+pip3.14 install rst2pdf
+pip3.14 install simple-sphinx-xml-sitemap
 # build
 sh build.sh
 mkdir /output -p
